80a2854491e43734e6a45735b5048ba9750d4e28
bezalel <bezalelp@uci.edu>
2018-05-11 16:59:49 -0700

    related data with extra ViewBag

--- XontosoUniversity/Models/Enrollment.cs
+++ XontosoUniversity/Models/Enrollment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace XontosoUniversity.Models
 {
@@ -17,6 +18,7 @@ namespace XontosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
